f0e90c5cf2a2e64f2bc897072fd0db52b5cd439f
Wildan S. Nahar <wildansnahar@gmail.com>
2016-12-10 22:43:41 +0700

    clean up codes

--- routes/participants.js
+++ routes/participants.js
@@ -1,12 +1,13 @@
 var express         = require('express')
 var app             = express()
 var session       	= require('express-session');
-var MongoStore    = require('connect-mongo')(session);
+var MongoStore      = require('connect-mongo')(session);
 var cookieParser    = require('cookie-parser');
 app.use(cookieParser());
+
 // models
-var Participant = require('../models/participant')
-var router      = express.Router()
+var Participant     = require('../models/participant')
+var router          = express.Router()
 
 router.use(session({
   secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
@@ -16,6 +17,9 @@ router.use(session({
   store: new MongoStore({ url: 'mongodb://localhost:27017/usercloud'})
   })
 );
+
+// middleware
+var auth = require('../middleware/auth')
 
 function isLoggedIn(req,res,next){
   if(!req.session.user){
