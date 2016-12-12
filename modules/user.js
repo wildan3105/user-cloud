25cc51e4c12cf1f2e792eb1326be521950cdc6c0
Wildan S. Nahar <wildansnahar@gmail.com>
2016-12-12 22:02:08 +0700

    refactor codes

--- modules/user.js
+++ modules/user.js
@@ -2,11 +2,88 @@ var express         = require('express')
 var app             = express()
 var session       	= require('express-session');
 var MongoStore      = require('connect-mongo')(session);
+var mongoose        = require('mongoose');
 var cookieParser    = require('cookie-parser');
 app.use(cookieParser());
 
 // models
 var Participant     = require('../models/participant')
+
+exports.loginAdmin = function(req,res){
+  var user = req.body.username;
+  var pass = req.body.password
+  if(user === 'wildan' && pass === 'wildan123'){
+    req.session.user = user;
+    res.send('logged in')
+  } else if (user !== 'wildan' && pass == 'wildan123') {
+    console.log('wrong username')
+    res.send('wrong username')
+  } else if (user === 'wildan' && pass !== 'wildan123') {
+    console.log('wrong password')
+    res.send('wrong password')
+  } else if (user !== 'wildan' && pass !== 'wildan123') {
+    console.log('wrong username and password')
+    res.send('wrong username and password')
+  } else {
+    console.log('please try again')
+    res.send('please try again')
+  }
+}
+
+exports.logoutAdmin = function(req,res){
+  delete req.session.user;
+  console.log('logged out successfully')
+  res.send('logged out');
+}
+
+exports.createUser = function(req,res){
+  var user = new Participant()
+  user.email    = req.body.email
+  user.password = req.body.password
+  Participant.findOne({email: user.email}, function(err, exist){
+    if(exist){
+      console.log('email exist!')
+      res.json({
+        "Status":"Error",
+        "Message":"Email exist!"
+      })
+      return err;
+    } else {
+      user.save(function(err){
+        if(err){
+          res.send(err)
+        }
+        else {
+          res.send('User created')
+        }
+      })
+    }
+  })
+}
+
+exports.updateUserByEmail = function(req,res){
+  var params = req.params.email
+  Participant.findOne({email: params}, function(err, user){
+    if(err){
+      res.json({'message':'no user found'})
+    } else {
+      user.email = req.body.email
+      user.save(function(err){
+        if(err){
+          res.send(err)
+        }
+        res.format({
+          json: function(){
+            res.send({message: 'Participant updated! New email : ' + user.email})
+          },
+          html: function(){
+            res.send('updated')
+          }
+        })
+      })
+    }
+  })
+}
 
 exports.findUserByEmail = function(req,res){
   var params = req.params.email;
--- routes/participants.js
+++ routes/participants.js
@@ -1,5 +1,4 @@
-
-gvar express         = require('express')
+var express         = require('express')
 var app             = express()
 var session       	= require('express-session');
 var MongoStore      = require('connect-mongo')(session);
@@ -34,49 +33,6 @@ function isLoggedIn(req,res,next){
   }
 }
 
-// test handler
-router.get('/test', isLoggedIn, function(req,res){
-  res.send('testing')
-})
-
-router.post('/login', function(req,res){
-  var user = req.body.username;
-  var pass = req.body.password
-  if(user === 'wildan' && pass === 'wildan123'){
-    req.session.user = user;
-    res.send('logged in')
-  } else if (user !== 'wildan' && pass == 'wildan123') {
-    console.log('wrong username')
-    res.send('wrong username')
-  } else if (user === 'wildan' && pass !== 'wildan123') {
-    console.log('wrong password')
-    res.send('wrong password')
-  } else if (user !== 'wildan' && pass !== 'wildan123') {
-    console.log('wrong username and password')
-    res.send('wrong username and password')
-  } else {
-    console.log('please try again')
-    res.send('please try again')
-  }
-})
-
-router.post('/logout', function(req, res){
-  delete req.session.user;
-  console.log('logged out successfully')
-  res.send('logged out');
-})
-
-// router to check session
-router.get('/check_session', function(req,res){
-  if (req.session.user = 'undefined'){
-    console.log('please login')
-    res.send('please login')
-  } else if(req.session.user == 'wildan') {
-    console.log('your session is ', req.session.user)
-    res.send('your session is ', req.session.user)
-  }
-})
-
 router.use(function(req, res, next){
   console.log('%s %s [%s]', req.method, req.url, res.statusCode.toString())
   next() // go to next route and not stop here
@@ -89,74 +45,21 @@ router.get('/', function(req,res, next){
   })
 })
 
-// list of functions
+// test handler
+router.get('/test', isLoggedIn, function(req,res){
+  res.send('testing')
+})
+
+router.post('/login', user.loginAdmin)
+router.post('/logout', user.logoutAdmin)
 
 router.use(isLoggedIn)
+
 router.get('/participants', user.findAll)
+
+router.post('/participant', user.createUser)
 router.get('/participant/:email', user.findUserByEmail)
 router.delete('/participant/:email', user.deleteByEmail)
-
-router.route('/participants')
-  // post single participant
-  .post(function(req,res){
-    var user = new Participant()
-    user.email    = req.body.email
-    user.password = req.body.password
-
-    Participant.findOne({email: user.email}, function(err, exist){
-      if(exist){
-        console.log('email exist!')
-        res.json({
-          "Status":"Error",
-          "Message":"Email exist!"
-        })
-        return err;
-      } else {
-        user.save(function(err){
-          if(err){
-            res.send(err)
-          }
-          else {
-            res.format({
-              json: function(){
-                res.send({
-                  "Status":"OK",
-                  "Message":"Participant created"
-                })
-              },
-              html: function(){
-                res.send("OK")
-              }
-            })
-          }
-        })
-      }
-    })
-  })
-
-router.route('/participant/:email')
-// update single participant
-.put(function(req, res){
-  Participant.findOne({email: req.params.email}, function(err, user){
-    if(err){
-      res.json({'message':'no user found'})
-    } else {
-      user.email = req.body.email
-      user.save(function(err){
-        if(err){
-          res.send(err)
-        }
-        res.format({
-          json: function(){
-            res.send({message: 'Participant updated! New email : ' + user.email})
-          },
-          html: function(){
-            res.send('updated')
-          }
-        })
-      })
-    }
-  })
-})
+router.put('/participant/:email', user.updateUserByEmail)
 
 module.exports = router;
