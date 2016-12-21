97f9f7654670e18a3dbf7620480dc8191aa2aa8e
Wildan S. Nahar <wildansnahar@gmail.com>
2016-12-22 00:58:25 +0700

    serve static files

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
+app.use('/static', express.static(path.join(__dirname, 'views/static')))
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html')
